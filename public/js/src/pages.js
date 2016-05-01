2243685feb5780b807c4aedaeb43abe50d47e9e6
Thomas Foricher <thomas.foricher@gmail.com>
2016-05-01 13:03:59 +0200

    Add responsive js functions

--- public/js/src/pages.js
+++ public/js/src/pages.js
@@ -7,5 +7,10 @@ pages: {
   index: function() {
     console.log("pages:index");
     // action-specific code
+
+    onResizeFunctions.index = function(w,h){
+      console.log('Do something specific for this page on resize', w, h);
+    }
+
   }
 },
