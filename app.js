4885b966b6282a483844cea2ad13de621e510250
codeKonami <thomas.foricher@gmail.com>
2016-06-07 19:24:09 +0200

    Change name modules to utils

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(session({ secret: config.secret, resave: true, saveUninitialized: true }));
-app.use(require('./modules/lang'));
+app.use(require('./utils/lang'));
 app.use(function(req,res,next){req.io = io;next();});
 app.use(require('./routes'));
