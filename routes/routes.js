45f7968d37bb431de688222c2e4ec5ab62120ac6
Thomas Foricher <thomas.foricher@gmail.com>
2016-09-06 10:54:41 +0200

    Clean robots.txt

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     sitemap = require('../utils/sitemap'),
     rss = require('../utils/rss'),
+    robots = require('../utils/robots'),
     config = require('../local-config'),
     router = express.Router(),
 /* GENUINE INCLUDE */
@@ -10,15 +11,7 @@ var express = require('express'),
 router.get('/', pages.index);
 /* GENUINE ROUTE */
 
-router.get('/robots.txt', function(req, res){
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  var response = "User-agent: Twitterbot\nDisallow:\n\n";
-  response += "User-agent: *\nDisallow:";
-  if(config.env == 'dev' || config.env == 'staging'){
-    response += ' /';
-  }
-  res.end(response);
-});
+router.get('/robots.txt', robots);
 
 router.get('/sitemap', sitemap);
 router.get('/sitemap.xml', sitemap);
